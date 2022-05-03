400e4f6a588e4b49ceee21e357f2c1317767afe7
b.minseok96@gmail.com <B.minseok96@gmail.com>
2022-05-03 21:58:01 +0900

    Add Header Setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-function App() {
+import React from 'react';
+import Header from './components/Layout/Header'
+const App = () => {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <React.Fragment>
+        <Header/>
+      </React.Fragment>
     </div>
   );
 }
