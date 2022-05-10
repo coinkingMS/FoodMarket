dc1fb02389ded0b26742e7d5573dd5334aeb9508
b.minseok96@gmail.com <B.minseok96@gmail.com>
2022-05-10 18:01:43 +0900

    add MealItem Ref

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,24 +1,41 @@
-import React,{useRef} from "react"
-import classes from "./MealItemForm.module.css"
-import Input from "../../UI/Input"
-const MealtemForm = () => {
-    const addValue = useRef(0);
-    const addChangeHandler = () => {
-        addValue.current = addValue.current + 1
-        console.log(addValue.current)
+import React, { useRef, useState } from "react";
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+const MealtemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const SubmitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
     }
-    return(
-        <form className={classes.form}>
-            <Input label="Amount" input={{
-                id: 'amount',
-                type: 'number',
-                min: '1',
-                max: '5',
-                step: '1',
-                defaultValue: '1'
-            }}/>
-            <button onClick = { addChangeHandler }>+Add</button>
-        </form>
-    )
-}
-export default MealtemForm
+    props.onAddToCart(enteredAmountNumber);
+  };
+  return (
+    <form className={classes.form} onSubmit={SubmitHandler}>
+      <Input
+        label="Amount"
+        ref={amountInputRef}
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>+Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
+    </form>
+  );
+};
+export default MealtemForm;
